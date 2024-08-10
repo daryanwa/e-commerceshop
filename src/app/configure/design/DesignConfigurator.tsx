9a34e18936ab4684271dd9aee288e417bf9839ef
Rostislav Parahnenko <asawww100@gmail.com>
2024-08-10 20:50:23 +0300

    buidupd

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -383,7 +383,7 @@ const DesignConfigurator = ({
               <Button
                 isLoading={isPending}
                 disabled={isPending}
-                loadingText="Saving"
+                isLoadingText="Saving"
                 onClick={() =>
                   saveConfig({
                     configId,
